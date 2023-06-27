e5927213a9e09901b7b4129cc61d504aeffd071a
Mekdes Hailu <123937792+MekdesHailu@users.noreply.github.com>
2023-06-27 19:10:12 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,5 @@
+!/bin/bash
+cat 0-main.c
 #include "main.h"
 #include <stdio.h>
 
@@ -16,3 +18,4 @@ int main(void)
     printf("n=%d\n", n);
     return (0);
 }
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-reset_to_98.c -o 0-98
